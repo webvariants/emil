9232ff44d8ee617ea716cfca0dd65553ef190def
Steven David <steven.david@webvariants.de>
2014-08-11 14:20:32 +0200

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+
+module.exports = function(grunt) {
+  // constants and settings
+  grunt.option('branch', 'default');
+
+  grunt.option('commandline-options', {
+    stdout: true,
+    stderr: true,
+    failOnError: true
+  });
+
+  // auto-loading
+  require('load-grunt-tasks')(grunt, {
+    pattern: ['grunt-*'],
+    config: 'package.json',
+    scope: ['devDependencies', 'dependencies']
+  });
+  require('time-grunt')(grunt);
+// grunt-newer
+// grunt-watch
+// grunt-notify
+
+  // grunt.option('commandline-options', {
+  //   stdout: true,
+  //   stderr: true,
+  //   failOnError: true
+  // });
+
+  // config
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  });
+
+  grunt.loadTasks(grunt.config('pkg.project.directories.build-tasks'));
+};
---
+++ build/tasks/version-control.js
@@ -0,0 +1,11 @@
+
+// log
+// changelog
+// commit / c
+// update / up
+// push / p
+// status / st
+
+module.exports = function(grunt) {
+
+};
