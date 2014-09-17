303fd5fb4a20459037bec7a60427800b847a0234
Steven David <steven.david@webvariants.de>
2014-09-17 09:28:19 +0200

    search table cell: no active state scale;

---
+++ vendor/jquery/faker.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $('.nav-table-cell #configuration').click(function() {
+        $('.nav-table-cell.configuration').toggleClass('open');
+        $('.nav-table-cell #configuration .icon').toggleClass('icon-64-interface-arrow-bottom');
+        $('.nav-table-cell #configuration .icon').toggleClass('icon-62-interface-arrow-top');
+    });
+
+
+    $('.header #header-info-button').click(function() {
+        $('.view #info-panel').toggleClass('shown');
+    });
+
+    $('.switch').click(function(){
+
+        $('#ntp').toggleClass('shown');
+        $('#timezone').toggleClass('shown');
+        $('#time').toggleClass('shown');
+        $('#date').toggleClass('shown');
+    });
+
+}); // End ready()
