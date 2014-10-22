85887adf88e923fbec9fb44566fff42a531a212d
Stephan Wirsich <stephan@wirsich.net>
2014-10-22 16:39:01 +0200

    Build: clean up and setup tasks

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo get npm dependencies
+npm install
+echo got npm dependencies
+
+echo get bower dependencies
+bower install
+echo got bower dependencies
+
+grunt setup
+
+exit $?
